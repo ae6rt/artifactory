3245baad26d03c3628759400702859e36a0ad68e
Mark Petrovic <mark.petrovic@xoom.com>
2016-05-01 06:29:11 -0700

    add test for remove-repo

---
+++ removesnapshotrepo_test.go
@@ -0,0 +1,31 @@
+package artifactory
+
+import (
+	"crypto/tls"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRemoveSnapshot(t *testing.T) {
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "DELETE" {
+			t.Fatalf("wanted DELETE but found %s\n", r.Method)
+		}
+		url := *r.URL
+		if url.Path != "/api/repositories/test-repo" {
+			t.Fatalf("want /api/repositories/test-repo but found %s\n", url.Path)
+		}
+		if r.Header.Get("Authorization") != "Basic dTpw" {
+			t.Fatalf("Want  Basic dTpw but found %s\n", r.Header.Get("Authorization"))
+		}
+		w.WriteHeader(200)
+	}))
+	defer testServer.Close()
+
+	client := NewBasicAuthClient("u", "p", testServer.URL, &tls.Config{})
+	_, err := client.RemoveRepository("test-repo")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
